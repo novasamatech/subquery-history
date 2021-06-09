ddc7a9606b25f318ed7a15e16a0034a0fbb2b2a6
Aleksandr Malina <ef1rspb@gmail.com>
2021-06-09 10:45:36 +0300

    Save from - to

--- src/mappings/Transfers.ts
+++ src/mappings/Transfers.ts
@@ -1,18 +1,39 @@
 import {HistoryElement} from '../types/models/HistoryElement';
 import {SubstrateEvent} from "@subql/types";
+import {Balance} from "@polkadot/types/interfaces";
+
+function eventId(event: SubstrateEvent): string {
+    return `${event.block.block.header.number.toString()}-${event.idx}`
+}
 
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
     const {event: {data: [from, to, amount]}} = event;
 
-    const element = new HistoryElement(event.block.block.header.number.toString() + event.idx);
+    const {event: {data: [, fee]}} = event.extrinsic.events.find((event) => {
+        return event.event.method == "Deposit" && event.event.section == "balances"
+    })
 
-    element.address = from.toString()
-    element.timestamp = event.block.timestamp.toISOString()
-    element.transfer = {
+    const elementFrom = new HistoryElement(eventId(event)+`-1`);
+    elementFrom.address = from.toString()
+    elementFrom.timestamp = event.block.timestamp.toISOString()
+    elementFrom.transfer = {
         amount: amount.toString(),
         from: from.toString(),
-        to: to.toString()
+        to: to.toString(),
+        fee: (fee as Balance).toString()
     }
+    await elementFrom.save();
 
-    await element.save();
+    const elementTo = new HistoryElement(eventId(event)+`-2`);
+    elementTo.address = to.toString()
+    elementTo.timestamp = event.block.timestamp.toISOString()
+
+
+    elementTo.transfer = {
+        amount: amount.toString(),
+        from: from.toString(),
+        to: to.toString(),
+        fee: (fee as Balance).toString()
+    }
+    await elementTo.save();
 }
