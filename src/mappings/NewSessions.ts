e28dbd1a02bd9d18ac5d1ccd3af830dec532180e
Aleksandr Malina <ef1rspb@gmail.com>
2021-06-18 16:56:44 +0300

    Create validators

--- src/mappings/NewSessions.ts
+++ src/mappings/NewSessions.ts
@@ -1,6 +1,6 @@
 import {SubstrateEvent} from "@subql/types";
 import {eventId} from "./common";
-import {EraStakersInfo, ValidatorExposure} from "../types";
+import {EraStakersInfo, EraValidatorInfo, IndividualExposure, ValidatorExposure} from "../types";
 
 export async function handleNewSession(event: SubstrateEvent): Promise<void> {
     const eraStakersInfo = new EraStakersInfo(eventId(event))
@@ -11,11 +11,22 @@ export async function handleNewSession(event: SubstrateEvent): Promise<void> {
 
     let erasStakers = await Promise.all(validators.map(id => api.query.staking.erasStakers(eraIndex, id)))
 
-    // erasStakers.forEach(exposure => {
-    //     let a = erasStakers.map(a => { own: a.own.toString, total: a.total.toString })
-    //     eraStakersInfo.exposure = a
-    // })
+    let validatorInfo = erasStakers.map(exp => {
+        return <EraValidatorInfo>{
+            accountId: "testId",
+            exposure: {
+                total: exp.total.toString(),
+                own: exp.own.toString(),
+                others: exp.others.map(other => {
+                    return <IndividualExposure> {
+                        who: other.who.toString(),
+                        value: other.value.toString()
+                    }
+                })
+            }
+        }
+    })
     eraStakersInfo.era = eraIndex.toString()
-    eraStakersInfo.validators = []
+    eraStakersInfo.validators = validatorInfo
     await eraStakersInfo.save()
 }
