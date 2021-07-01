898036242408ecca64f49ba336b798b35258cf0e
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-01 07:44:42 +0300

    Store Stake data (#6)
    * Fetch ErasValidatorInfo
    
    * Create validators
    
    * Add validator id
    
    * Fix and optimize stake fetching
    
    * Set start block to 1
    
    Co-authored-by: valentun <sergeev@soramitsu.co.jp>

---
+++ src/mappings/NewEra.ts
@@ -0,0 +1,34 @@
+import {SubstrateEvent} from "@subql/types";
+import {eventId} from "./common";
+import {EraStakersInfo} from "../types";
+
+export async function handleNewEra(event: SubstrateEvent): Promise<void> {
+    const eraStakersInfo = new EraStakersInfo(eventId(event))
+
+    let eraOption = await api.query.staking.activeEra()
+    let eraIndex = eraOption.unwrap().index
+
+    eraStakersInfo.era = eraIndex.toString()
+
+    const exposures = await api.query.staking.erasStakers.entries(eraIndex);
+
+    eraStakersInfo.validators = exposures.map(([key, exposure]) => {
+        const [, validatorId] = key.args
+
+        return {
+            accountId: validatorId.toString(),
+            exposure: {
+                total: exposure.total.toString(),
+                own: exposure.own.toString(),
+                others: exposure.others.map(other => {
+                    return {
+                        who: other.who.toString(),
+                        value: other.value.toString()
+                    }
+                })
+            }
+        }
+    })
+
+    await eraStakersInfo.save()
+}
