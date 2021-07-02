6d30f4baae1123aa9f6e7b8040feed7e39f59dd9
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-02 10:49:10 +0300

    Define StakeChanged type

--- src/mappings/StakeChanged.ts
+++ src/mappings/StakeChanged.ts
@@ -1,15 +1,16 @@
 import {SubstrateEvent} from "@subql/types";
-import {Bonded, HistoryElement} from "../types";
+import {StakeChange} from "../types";
 import {eventId, timestamp} from "./common";
 import {Balance} from "@polkadot/types/interfaces";
 
 export async function handleBonded(event: SubstrateEvent): Promise<void> {
     const {event: {data: [stash, amount]}} = event;
 
-    const element = new Bonded(eventId(event));
+    const element = new StakeChange(eventId(event));
     element.timestamp = timestamp(event.extrinsic.block)
-    element.stash = stash.toString()
+    element.address = stash.toString()
     element.amount = (amount as Balance).toString()
+    element.type = "bonded"
 
     await element.save()
 }
