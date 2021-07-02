52dcb578dede1a6cdcc00a3234327bdbe58581b6
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-02 10:36:39 +0300

    Define Bonded entity

--- src/mappings/Bonded.ts
+++ src/mappings/Bonded.ts
@@ -1,17 +1,15 @@
 import {SubstrateEvent} from "@subql/types";
-import {HistoryElement} from "../types";
+import {Bonded, HistoryElement} from "../types";
 import {eventId, timestamp} from "./common";
 import {Balance} from "@polkadot/types/interfaces";
 
 export async function handleBonded(event: SubstrateEvent): Promise<void> {
     const {event: {data: [stash, amount]}} = event;
 
-    const element = new HistoryElement(eventId(event));
-    element.address = event.extrinsic.extrinsic.signer.toString()
+    const element = new Bonded(eventId(event));
     element.timestamp = timestamp(event.extrinsic.block)
-    element.bonded = {
-        stash: stash.toString(),
-        amount: (amount as Balance).toString()
-    }
+    element.stash = stash.toString()
+    element.amount = (amount as Balance).toString()
+
     await element.save()
 }
