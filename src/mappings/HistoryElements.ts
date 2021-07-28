d6ba260fbbaf70283c50437c6059691817597481
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-28 09:36:21 +0300

    Extract failed Transfers from batch

--- src/mappings/HistoryElements.ts
+++ src/mappings/HistoryElements.ts
@@ -1,57 +1,108 @@
-import { SubstrateExtrinsic } from '@subql/types';
+import {SubstrateEvent, SubstrateExtrinsic} from '@subql/types';
 import {HistoryElement, Transfer} from "../types";
 import {
-    blockNumber,
+    blockNumber, callFromProxy, callsFromBatch, eventId,
     eventIdFromBlockAndIdx,
     exportFeeFromDepositEvent,
-    extrinsicId,
+    extrinsicId, isBatch, isProxy,
     isTransfer,
     timestamp
 } from "./common";
 import {Balance} from "@polkadot/types/interfaces";
+import {CallBase} from "@polkadot/types/types/calls";
+import {AnyTuple} from "@polkadot/types/types/codec";
 
 export async function handleHistoryElement(extrinsic: SubstrateExtrinsic): Promise<void> {
     const { isSigned } = extrinsic.extrinsic;
     if (isSigned) {
-        const element = new HistoryElement(extrinsic.extrinsic.hash.toString());
-        element.address = extrinsic.extrinsic.signer.toString()
-        element.timestamp = timestamp(extrinsic.block)
-
-        let transfer = findTransferCall(extrinsic)
-        if (transfer != undefined) {
-            element.transfer = transfer
+        let failedTransfers = findFailedTransferCalls(extrinsic)
+        if (failedTransfers != null) {
+            await saveFailedTransfers(failedTransfers, extrinsic)
         } else {
-            element.extrinsic = {
-                hash: extrinsic.extrinsic.hash.toString(),
-                module: extrinsic.extrinsic.method.section,
-                call: extrinsic.extrinsic.method.method,
-                success: extrinsic.success,
-                fee: exportFeeFromDepositEvent(extrinsic)?.toString()
-            }
+            await saveExtrinsic(extrinsic)
         }
-
-        await element.save()
     }
 }
 
-function findTransferCall(extrinsic: SubstrateExtrinsic): Transfer | null {
-    if (!isTransfer(extrinsic.extrinsic.method)) {
+async function saveFailedTransfers(transfers: Transfer[], extrinsic: SubstrateExtrinsic): Promise<void> {
+    let promises = transfers.map(transfer => {
+        const elementFrom = new HistoryElement(transfer.extrinsicId+`-from`);
+        elementFrom.address = transfer.from
+        elementFrom.timestamp = timestamp(extrinsic.block)
+        elementFrom.transfer = transfer
+
+        const elementTo = new HistoryElement(transfer.extrinsicId+`-to`);
+        elementTo.address = transfer.to
+        elementTo.timestamp = timestamp(extrinsic.block)
+        elementTo.transfer = transfer
+
+        return [elementTo.save(), elementFrom.save()]
+    })
+    await Promise.allSettled(promises)
+}
+
+async function saveExtrinsic(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const element = new HistoryElement(extrinsic.extrinsic.hash.toString());
+    element.address = extrinsic.extrinsic.signer.toString()
+    element.timestamp = timestamp(extrinsic.block)
+    element.extrinsic = {
+        hash: extrinsic.extrinsic.hash.toString(),
+        module: extrinsic.extrinsic.method.section,
+        call: extrinsic.extrinsic.method.method,
+        success: extrinsic.success,
+        fee: exportFeeFromDepositEvent(extrinsic)?.toString()
+    }
+    await element.save()
+}
+
+/// Success Transfer emits Transfer event that is handled at Transfers.ts handleTransfer()
+function findFailedTransferCalls(extrinsic: SubstrateExtrinsic): Transfer[] | null {
+    if (extrinsic.success) {
         return null;
     }
 
-    let args = extrinsic.extrinsic.args
-    let destination = args[0]
-    let amount = (args[1] as Balance).toBigInt()
-    let sender = extrinsic.extrinsic.signer
-
-    let blockNumber = extrinsic.block.block.header.number.toString()
-    return {
-        amount: amount.toString(),
-        from: sender.toString(),
-        to: destination.toString(),
-        block: blockNumber,
-        fee: exportFeeFromDepositEvent(extrinsic).toString(),
-        extrinsicId: eventIdFromBlockAndIdx(blockNumber, extrinsic.idx.toString()),
-        success: extrinsic.success
+    let transferCallsArgs = determineTransferCallsArgs(extrinsic.extrinsic.method)
+    if (transferCallsArgs.length == 0) {
+        return null;
     }
+
+    let sender = extrinsic.extrinsic.signer
+    return transferCallsArgs.map(tuple => {
+        let blockNumber = extrinsic.block.block.header.number.toString()
+        return {
+            amount: tuple[0],
+            from: sender.toString(),
+            to: tuple[1].toString(),
+            block: blockNumber,
+            fee: exportFeeFromDepositEvent(extrinsic).toString(),
+            extrinsicId: eventIdFromBlockAndIdx(blockNumber, extrinsic.idx.toString()),
+            success: false
+        }
+    })
+}
+
+function determineTransferCallsArgs(causeCall: CallBase<AnyTuple>) : [string, number][] {
+    if (isTransfer(causeCall)) {
+        return [extractArgsFromTransfer(causeCall)]
+    } else if (isBatch(causeCall)) {
+        return callsFromBatch(causeCall)
+            .map(call => {
+                return determineTransferCallsArgs(call)
+                    .map((value, index, array) => {
+                        return value
+                    })
+            })
+            .flat()
+    } else if (isProxy(causeCall)) {
+        let proxyCall = callFromProxy(causeCall)
+        return determineTransferCallsArgs(proxyCall)
+    } else {
+        return []
+    }
+}
+
+function extractArgsFromTransfer(call: CallBase<AnyTuple>): [string, number] {
+    const [destinationAddress, amount] = call.args
+
+    return [destinationAddress.toString(), (amount as Balance).toNumber()]
 }
