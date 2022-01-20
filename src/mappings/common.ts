d217acd1c24315ad14586e8d1f15977b7988fb0c
asmadek <alma2610@ya.ru>
2022-01-20 16:20:18 +0500

    ISSUE-39: Fee calculation with withdraw

--- src/mappings/common.ts
+++ src/mappings/common.ts
@@ -59,6 +59,11 @@ export function timestamp(block: SubstrateBlock): bigint {
 
 export function calculateFeeAsString(extrinsic?: SubstrateExtrinsic): string {
     if (extrinsic) {
+        const withdrawFee = exportFeeFromBalancesWithdrawEvent(extrinsic)
+        if (withdrawFee !== BigInt(0)) {
+            return withdrawFee.toString();
+        }
+
         let balancesFee = exportFeeFromBalancesDepositEvent(extrinsic)
         let treasureFee = exportFeeFromTreasureDepositEvent(extrinsic)
 
@@ -67,6 +72,27 @@ export function calculateFeeAsString(extrinsic?: SubstrateExtrinsic): string {
     } else {
         return BigInt(0).toString()
     } 
+}
+
+function exportFeeFromBalancesWithdrawEvent(extrinsic: SubstrateExtrinsic): bigint {
+    const eventRecord = extrinsic.events.find((event) => {
+        return event.event.method == "Withdraw" && event.event.section == "balances"
+    })
+    
+    if (eventRecord !== undefined) {
+        const {
+            event: {
+                data: [ accountid, fee ]
+            }
+        } = eventRecord
+
+        const extrinsicSigner = extrinsic.extrinsic.signer.toString()
+        const withdrawAccountId = accountid.toString()
+        
+        return extrinsicSigner === withdrawAccountId ? (fee as Balance).toBigInt() :  BigInt(0)
+    }
+
+    return BigInt(0)
 }
 
 function exportFeeFromBalancesDepositEvent(extrinsic: SubstrateExtrinsic): bigint {
@@ -78,9 +104,9 @@ function exportFeeFromBalancesDepositEvent(extrinsic: SubstrateExtrinsic): bigin
         const {event: {data: [, fee]}}= eventRecord
 
         return (fee as Balance).toBigInt()
-    } else  {
-        return BigInt(0)
     }
+
+    return BigInt(0)
 }
 
 function exportFeeFromTreasureDepositEvent(extrinsic: SubstrateExtrinsic): bigint {
