37e5123c6f9242c912e00f05609a90805587e541
asmadek <alma2610@ya.ru>
2022-02-07 16:27:07 +0500

    Add types for Kintsugi

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -1,9 +1,39 @@
-import interbtc from '@interlay/interbtc-types';
+import definitions, { RpcFunctionDefinition } from "@interlay/interbtc-types";
+import { OverrideBundleType } from '@polkadot/types/types';
+
+const types = {
+  types: [
+    {
+      minmax: [0, undefined],
+      types: definitions.types[0].types
+    }
+  ],
+  rpc: definitions.rpc,
+  instances: definitions.instances,
+}
+
+const typesBundle: OverrideBundleType = {
+  spec: {
+    interlay: types,
+    interbtc: types,
+    kintsugi: types
+  },
+}
 
 export default {
-  typesBundle: {
-    spec: {
-      interbtc
-    }
-  }
+  types: {
+    GenericMultiAddress: {
+      _enum: {
+        Id: 'AccountId',
+        Index: 'Compact<AccountIndex>',
+        Raw: 'Bytes',
+        Address32: 'H256',
+        Address20: 'H160'  
+      }
+    },
+    MultiAddress: "GenericMultiAddress",
+    Address: "MultiAddress",
+    LookupSource: "Address"
+  },
+  typesBundle
 };
