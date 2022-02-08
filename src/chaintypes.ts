bd8b2d2618102ca51c08f06c68ed16e982226549
asmadek <alma2610@ya.ru>
2022-02-08 12:47:29 +0500

    Fix types for kintsugi

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -20,23 +20,8 @@ const typesBundle: OverrideBundleType = {
 
 export default {
   types: {
-    H160: "[u8; 20]",
-    H256: "[u8; 32]",
-    AccountIndex: "u32",
-    AccountId: "u64",
-    GenericMultiAddress: {
-      _enum: {
-        Id: "AccountId",
-        Index: "Compact<AccountIndex>",
-        Raw: "Bytes",
-        Address32: "H256",
-        Address20: "H160",
-      },
-    },
-    MultiAddress: "GenericMultiAddress",
-    LookupSource: "Address",
-    Index: "u32",
-    ...definitions.types[0].types,
+    AccountId32: "[u8; 32]",
+    Address: "AccountId32",
   },
   typesBundle,
 };
