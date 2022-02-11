1bb7ed1e50c0fb64271197fdfc5e0351f119ba45
asmadek <alma2610@ya.ru>
2022-02-11 16:15:44 +0500

    Init picasso project

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -6,25 +6,6 @@ const types = {
       // on all versions
       minmax: [0, undefined],
       types: {
-        Record: 'Vec<u8>',
-        Technics: 'Vec<u8>',
-        Economics: '{}',
-        Report: {
-          index: 'LiabilityIndex',
-          sender: 'AccountId',
-          payload: 'Vec<u8>',
-          signature: 'MultiSignature'
-        },
-        ReportFor: 'Report',
-        Agreement: {
-          technics: 'Technics',
-          economics: 'Economics',
-          promisee: 'AccountId',
-          promisor: 'AccountId',
-          promisee_signature: 'MultiSignature',
-          promisor_signature: 'MultiSignature'
-        },
-        LiabilityIndex: 'u32'
       }
     }
   ]
@@ -32,7 +13,7 @@ const types = {
 
 const typesBundle: OverrideBundleType = {
   spec: {
-    robonomics: types,
+    picasso: types,
   },
 }
 
