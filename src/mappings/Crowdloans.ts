842714da92e3ec50fb59651d55c46c3be0147ee0
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-05-21 23:44:08 +0300

    fixes after testing

--- src/mappings/Crowdloans.ts
+++ src/mappings/Crowdloans.ts
@@ -1,10 +1,10 @@
-import { AccumulatedReward, CrowdloanContribute, AccumulatedContribute } from '../types';
-import { SubstrateEvent } from "@subql/types";
+import {CrowdloanContribute, AccumulatedContribute} from '../types';
+import {SubstrateEvent} from "@subql/types";
 import {
     timestamp,
     eventId,
 } from "./common";
-import { Balance } from "@polkadot/types/interfaces";
+import {Balance} from "@polkadot/types/interfaces";
 
 
 export async function handleContributed(event: SubstrateEvent): Promise<void> {
@@ -15,56 +15,60 @@ export async function handleContributed(event: SubstrateEvent): Promise<void> {
 async function handleContribute(event: SubstrateEvent): Promise<void> {
     await handleContributeForContributionHistory(event)
     await updateAccumulatedContribution(event)
-    Promise.resolve()
 }
 
 
 async function handleContributeForContributionHistory(event: SubstrateEvent) {
     const contribute = new CrowdloanContribute(eventId(event))
-    logger.info(contribute)
     const block = event.block;
     const blockNumber = block.block.header.number.toNumber()
     const blockTimestamp = timestamp(block)
+    const { event: { data: [account, parachainId, amount] } } = event
+
     contribute.blockNumber = blockNumber
     contribute.timestamp = blockTimestamp
-    const { event: { data: [account, para_id, amount] } } = event
     contribute.amount = (amount as Balance).toBigInt()
-    contribute.parachain_id = para_id.toString()
+    contribute.parachainId = parachainId.toString()
     contribute.address = account.toString()
+
     await contribute.save()
 }
 
 async function updateAccumulatedContribution(event: SubstrateEvent) {
-    let { event: { data: [accountId, parachain_id, amount] } } = event
+    let { event: { data: [accountId, parachainId, amount] } } = event
+    const bigIntAmount = (amount as Balance).toBigInt()
+
     let accountAddress = accountId.toString()
 
     let accumulatedContribute = await AccumulatedContribute.get(accountAddress);
-    if (!accumulatedContribute) {
-        accumulatedContribute = new AccumulatedContribute(accountAddress);
-        accumulatedContribute.amount = BigInt(0)
-        accumulatedContribute.parachains = [{
-            parachain_id: parachain_id.toString(),
-            amount: (amount as Balance).toBigInt()
-        }]
-    }
-    const exist_parachain = accumulatedContribute.parachains.filter(args => args.parachain_id == parachain_id.toString())
 
-    if (exist_parachain) {
-        accumulatedContribute.parachains
-            .filter(args => args.parachain_id == parachain_id.toString())
-            .map(parachain => parachain.amount += (amount as Balance).toBigInt())
+    const exist_parachain = accumulatedContribute?.parachains?.find(args => args.parachainId == parachainId.toString())
+
+    if (exist_parachain) { //Check that account already has a contribution for that parachain
+
+        let parachain = accumulatedContribute.parachains
+            .find(args => args.parachainId == parachainId.toString())
+        parachain.amount = (BigInt(parachain.amount) + bigIntAmount).toString()
+
     } else {
-        accumulatedContribute.parachains
-            .filter(args => args.parachain_id == parachain_id.toString())
-            .push(
+
+        if (!accumulatedContribute) { //Check that account already has a contribution for someone parachain
+            accumulatedContribute = new AccumulatedContribute(accountAddress);
+            accumulatedContribute.totalContributionAmount = BigInt(0)
+            accumulatedContribute.parachains = [{
+                parachainId: parachainId.toString(),
+                amount: bigIntAmount.toString()
+            }]
+        } else { //If account already has a contribution to another parachain will add current parachain in the parachains array
+            accumulatedContribute.parachains.push(
                 {
-                    parachain_id: parachain_id.toString(),
-                    amount: (amount as Balance).toBigInt()
+                    parachainId: parachainId.toString(),
+                    amount: bigIntAmount.toString()
                 }
             )
+        }
     }
 
-    const newAmount = (amount as Balance).toBigInt()
-    accumulatedContribute.amount = accumulatedContribute.amount + newAmount
+    accumulatedContribute.totalContributionAmount = accumulatedContribute.totalContributionAmount + bigIntAmount
     await accumulatedContribute.save()
 }
