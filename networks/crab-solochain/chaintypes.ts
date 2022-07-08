da5f26ed72cb33ba50dc1a9ce969f8a756d2cc8c
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-07-08 11:32:18 +0300

    delete crab-solochain

--- networks/crab-solochain/chaintypes.ts
+++
@@ -1,6 +0,0 @@
-import { OverrideBundleType } from '@polkadot/types/types';
-import { typesBundleForPolkadotApps } from '@darwinia/types/mix';
-
-export default {
-  typesBundle: typesBundleForPolkadotApps as OverrideBundleType
-};
