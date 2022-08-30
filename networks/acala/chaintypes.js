25726bc82a0b06abf3ef61e0411b34562c7d77e6
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-08-30 14:07:12 +0300

    first iteration

---
+++ networks/acala/chaintypes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const type_definitions_1 = require("@acala-network/type-definitions");
+exports.default = {
+    typesBundle: type_definitions_1.typesBundleForPolkadot
+};
--- networks/acala/chaintypes.ts
+++
@@ -1,5 +0,0 @@
-import { typesBundleForPolkadot as typesBundle } from '@acala-network/type-definitions';
-
-export default {
-  typesBundle
-};
