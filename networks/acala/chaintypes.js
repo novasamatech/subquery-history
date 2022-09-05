00c192a30d85fdad23332ccc7865c4d634284418
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-09-05 09:32:13 +0300

    Acala in separate directory

--- networks/acala/chaintypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const type_definitions_1 = require("@acala-network/type-definitions");
-exports.default = {
-    typesBundle: type_definitions_1.typesBundleForPolkadot
-};
