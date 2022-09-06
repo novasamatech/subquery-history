4de3e5adca9b2a9e0af607e81e995c71b44ecded
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-09-06 11:17:49 +0300

    change to matrix deployment

--- .github/get_all_project_files.sh
+++ .github/get_all_project_files.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 MAIN_DIRECTORY='.'
-arrVar=()
+
+JSON="{\"include\":["
 
 for item in "$MAIN_DIRECTORY"/*.yaml
 do
     # Skip base project.yaml which are using for initialization
-    if [[ "$entry" == "./project.yaml" ]]; then
+    if [[ "$item" == "./project.yaml" ]]; then
         continue
     fi
 
     arrIN=(${item//\// })
-    arrVar+=${arrIN[1]}' '
 
-done
+    JSONline="{\"project_file\": \"${arrIN[1]}\"},"
 
-echo $arrVar
+    if [[ "$JSON" != *"$JSONline"* ]]; then
+        JSON="$JSON$JSONline"
+    fi
+
+done <<< "$DIFF"
+
+# Remove last "," and add closing brackets
+if [[ $JSON == *, ]]; then
+    JSON="${JSON%?}"
+fi
+JSON="$JSON]}"
+
+echo $JSON
