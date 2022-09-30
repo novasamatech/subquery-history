cabdc920b50ac969aad40957997f8860dd43a182
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2022-09-30 15:48:14 +0500

    Adding Darwinia, Kapex, Kylin (#177)
    * Adding Kylin
    
    * Adding Darwinia parachain
    
    * Adding Kapex
    
    * fix Darwinia start block
    
    * Adding Darwinia cid
    
    * Update kapex deployment
    
    * update kylin deployment

---
+++ chainTypes/kylinChainTypes.ts
@@ -0,0 +1,36 @@
+import { OverrideBundleType } from "@polkadot/types/types";
+
+const types = {
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        Address: 'MultiAddress',
+        LookupSource: 'MultiAddress',
+        DataRequest: {
+          para_id: 'Option<ParaId>',
+          account_id: 'Option<AccountId>',
+          requested_block_number: 'BlockNumber',
+          processed_block_number: 'Option<BlockNumber>',
+          requested_timestamp: 'u128',
+          processed_timestamp: 'Option<u128>',
+          payload: 'Text',
+          feed_name: 'Text',
+          is_query: 'bool',
+          url: 'Option<Text>'
+        }
+      }
+    }
+  ]
+}
+
+const typesBundle: OverrideBundleType = {
+  spec: {
+    "kylin": types,
+  },
+};
+
+export default {
+  typesBundle,
+};
