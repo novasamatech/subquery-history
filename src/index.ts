9d1bd627b61df137ccf9ac7739b57e6ba29a8946
Vladimir Kukharenko <kukharenko.va@phystech.edu>
2023-06-11 13:11:17 +0200

    Stake dependencies deleted

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export * from './mappings/HistoryElements'
 export * from './mappings/Rewards'
 export * from './mappings/Transfers'
 export * from './mappings/NewEra'
-export * from './mappings/StakeChanged'
 import "@polkadot/api-augment"
