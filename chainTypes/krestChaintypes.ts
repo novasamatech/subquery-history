09e93a9f32184e44d7bc59759e8f7d556f288a83
vovunku <vovunku@mail.ru>
2023-08-25 09:49:02 +0200

    add base version with peaq chaintypes(doesn't work now)

---
+++ chainTypes/krestChaintypes.ts
@@ -0,0 +1,7 @@
+// "Adapted from https://github.com/polkadot-js/apps/blob/master/packages/apps-config/src/api/spec/peaq.ts
+
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
+import { typesBundleForPolkadot } from '@peaqnetwork/type-definitions';
+
+export default (typesBundleForPolkadot as { spec: { peaq: OverrideBundleDefinition } }).spec.peaq;
