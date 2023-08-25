d5253f17051742bdb714641a4791db2af21d7d39
vovunku <vovunku@mail.ru>
2023-08-25 10:35:38 +0200

    fixed chainTypes problem(typesBundle added)

--- chainTypes/frequencyChaintypes.ts
+++ chainTypes/frequencyChaintypes.ts
@@ -1,16 +1,20 @@
+// "Adapted from https://github.com/polkadot-js/apps/blob/master/packages/apps-config/src/api/spec/frequency.ts
+
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
 import { rpc, runtime, signedExtensions, types } from '@frequency-chain/api-augment';
 
 export default {
-  rpc,
-  runtime,
-  signedExtensions,
-  types: [
-    {
-      // on all versions
-      minmax: [0, undefined],
-      types
-    }
-  ]
-} as OverrideBundleDefinition;
+  typesBundle: {
+    rpc,
+    runtime,
+    signedExtensions,
+    types: [
+      {
+        // on all versions
+        minmax: [0, undefined],
+        types
+      }
+    ]
+  } as OverrideBundleDefinition
+};
