156e423f363971818128e280342e06e9cec55864
vovunku <vovunku@mail.ru>
2023-11-10 20:49:49 +0100

    added fix via chaintypes

---
+++ chainTypes/statemineChaintypes.ts
@@ -0,0 +1,30 @@
+import { OverrideBundleDefinition } from "@polkadot/types/types";
+
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      minmax: [0, 999999],
+      types: {
+        NovaAssetId: "Option<AssetId>"
+      },
+    },
+    {
+      minmax: [1000000, null],
+      types: {
+        NovaAssetId: "Option<MultiLocation>"
+      },
+    },
+  ],
+  signedExtensions: {
+    ChargeAssetTxPayment: {
+      extrinsic: {
+        tip: 'Compact<Balance>',
+        // eslint-disable-next-line sort-keys
+        assetId: 'NovaAssetId'
+      },
+      payload: {}
+    }
+  }
+};
+
+export default { typesBundle: { spec: { "statemine": definitions } } };
