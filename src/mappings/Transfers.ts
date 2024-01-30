b7048d1fa7ebacab50fe8d543e589a481fda4873
valentun <vallentunn@gmail.com>
2024-01-30 13:09:52 +0300

    Hydra swaps

--- src/mappings/Transfers.ts
+++ src/mappings/Transfers.ts
@@ -27,66 +27,6 @@ type TransferPayload = {
   assetId?: string;
 };
 
-export async function handleSwap(event: SubstrateEvent): Promise<void> {
-  const [from, to, path, amountIn, amountOut] = getEventData(event);
-
-  let element = await HistoryElement.get(`${eventId(event)}-from`)
-
-  if (element !== undefined) {
-      // already processed swap previously
-      return;
-  }
-
-  let assetIdFee: string
-  let fee: string
-  let foundAssetTxFeePaid = event.block.events.find((e) => isAssetTxFeePaidEvent(eventRecordToSubstrateEvent(e)));
-  let swaps = event.block.events.filter((e) => isSwapExecutedEvent(eventRecordToSubstrateEvent(e)));
-  if (foundAssetTxFeePaid === undefined) {
-    assetIdFee = "native"
-    fee = calculateFeeAsString(event.extrinsic, from.toString())
-  } else {
-    const [who, actualFee, tip, rawAssetIdFee] = getEventData(eventRecordToSubstrateEvent(foundAssetTxFeePaid))
-    assetIdFee = getAssetIdFromMultilocation(rawAssetIdFee)
-    fee = actualFee.toString()
-
-    let { event: { data: [feeFrom, feeTo, feePath, feeAmountIn, feeAmountOut] } } = swaps[0]
-
-    swaps = swaps.slice(1)
-    if (BigIntFromCodec(actualFee) != BigIntFromCodec(feeAmountIn)) {
-      let { event: { data: [refundFrom, refundTo, refundPath, refundAmountIn, refundAmountOut] } } = swaps[swaps.length - 1]
-
-      if (BigIntFromCodec(feeAmountIn) == BigIntFromCodec(actualFee) + BigIntFromCodec(refundAmountOut) && 
-         getAssetIdFromMultilocation(feePath[0]) == getAssetIdFromMultilocation(refundPath[refundPath["length"] - 1])) {
-          swaps = swaps.slice(swaps.length - 1)
-          // TODO: if fee splitted, than we will process the same block two times
-      }
-    }
-  }
-  await Promise.all(swaps.map((e) => processSwap(eventRecordToSubstrateEvent(e), assetIdFee, fee)))
-}
-
-async function processSwap(event: SubstrateEvent, assetIdFee: string, fee: string): Promise<void> {
-  const [from, to, path, amountIn, amountOut] = getEventData(event)
-
-  const swap = {
-    assetIdIn: getAssetIdFromMultilocation(path[0]),
-    amountIn: amountIn.toString(),
-    assetIdOut: getAssetIdFromMultilocation(path[path["length"] - 1]),
-    amountOut: amountOut.toString(),
-    sender: from.toString(),
-    receiver: to.toString(),
-    assetIdFee: assetIdFee,
-    fee: fee,
-    eventIdx: event.idx,
-    success: true
-  }
-
-  await createAssetTransmission(event, from.toString(), "-from", {"swap": swap});
-  if (from.toString() != to.toString()) {
-    await createAssetTransmission(event, to.toString(), "-to", {"swap": swap});
-  }
-}
-
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
   const [from, to, amount] = getEventData(event);
 
@@ -221,7 +161,7 @@ async function createTransfer({
   await createAssetTransmission(event, address, suffix, data);
 }
 
-async function createAssetTransmission(
+export async function createAssetTransmission(
   event,
   address,
   suffix,
---
+++ src/mappings/swaps/HydraDx.ts
@@ -0,0 +1,106 @@
+import {SubstrateEvent, TypedEventRecord} from "@subql/types";
+import {
+    eventId,
+    eventRecordToSubstrateEvent, extractTransactionPaidFee, getEventData,
+    isCurrencyDepositedEvent
+} from "../common";
+import {HistoryElement} from "../../types";
+import {createAssetTransmission} from "../Transfers";
+import {AccountId32} from "@polkadot/types/interfaces/runtime";
+import {u128, u32} from "@polkadot/types-codec";
+import {EventRecord} from "@polkadot/types/interfaces";
+import {Codec} from "@polkadot/types/types";
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+
+type OmnipoolSwapArgs = [who: AccountId32, assetIn: u32, assetOut: u32, amountIn: u128, amountOut: u128, assetFeeAmount: u128, protocolFeeAmount: u128]
+
+export async function handleOmnipoolSwap(event: SubstrateEvent<OmnipoolSwapArgs>): Promise<void> {
+    let element = await HistoryElement.get(`${eventId(event)}-from`)
+    if (element !== undefined) {
+        // already processed swap previously
+        return;
+    }
+    if (event.extrinsic == undefined) {
+        // TODO we dont yet process swap events that were initiated by the system and not by the user
+        // Example: https://hydradx.subscan.io/block/4361343?tab=event&event=4361343-27
+        return;
+    }
+
+    const fee = findHydraDxFeeTyped(event.extrinsic.events)
+    const [who, assetIn, assetOut, amountIn, amountOut] = event.event.data
+
+    const swap = {
+        assetIdIn: convertHydraDxTokenIdToString(assetIn),
+        amountIn: amountIn.toString(),
+        assetIdOut: convertHydraDxTokenIdToString(assetOut),
+        amountOut: amountOut.toString(),
+        sender: who,
+        receiver: who,
+        assetIdFee: fee.tokenId,
+        fee: fee.amount,
+        eventIdx: event.idx,
+        success: true
+    }
+
+    logger.info(`Constructed swap ${JSON.stringify(swap)}`)
+
+    await createAssetTransmission(event, who.toString(), "-from", {"swap": swap});
+}
+
+export type Fee = {
+    tokenId: string
+    amount: string
+}
+
+export function findHydraDxFeeTyped(events: TypedEventRecord<Codec[]>[]): Fee | undefined {
+    return findHydraDxFee(events as EventRecord[])
+}
+
+export function findHydraDxFee(events: EventRecord[]): Fee | undefined {
+    const lastCurrenciesDepositEvent = findLastEvent(events,
+        (event) => isCurrencyDepositedEvent(eventRecordToSubstrateEvent(event))
+    )
+
+    if (lastCurrenciesDepositEvent == undefined) return findNativeFee(events)
+
+    const {
+        event: {
+            data: [currencyId, _, amount]
+        }
+    } = lastCurrenciesDepositEvent
+
+    return {
+        tokenId: convertHydraDxTokenIdToString(currencyId),
+        amount: (amount as INumber).toString()
+    }
+}
+
+function findNativeFee(events: EventRecord[]): Fee | undefined {
+    let foundAssetTxFeePaid = extractTransactionPaidFee(events)
+    if (foundAssetTxFeePaid == undefined) return undefined
+
+    return {
+        tokenId: "native",
+        amount: foundAssetTxFeePaid
+    }
+}
+
+export function convertHydraDxTokenIdToString(hydraDxTokenId: Codec): string {
+    const asString = hydraDxTokenId.toString()
+
+    if (asString == "0") {
+        return "native"
+    } else {
+        return hydraDxTokenId.toHex(true)
+    }
+}
+
+function findLastEvent(events: EventRecord[], expression: (event: EventRecord) => boolean): EventRecord | undefined {
+    const currenciesDepositedEvents = events.filter((e) => isCurrencyDepositedEvent(eventRecordToSubstrateEvent(e)))
+
+    if (currenciesDepositedEvents.length == 0) {
+        return undefined
+    }
+
+    return currenciesDepositedEvents[currenciesDepositedEvents.length - 1]
+}
