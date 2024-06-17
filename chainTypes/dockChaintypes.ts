ec2b0833f84eb1541e7e4bddc6f3b164fd1eab46
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-06-17 11:07:28 +0300

    fix: dock chaintypes

--- chainTypes/dockChaintypes.ts
+++ chainTypes/dockChaintypes.ts
@@ -1,5 +1,5 @@
 // Adapted from https://github.com/polkadot-js/apps/blob/master/packages/apps-config/src/api/spec/dock-mainnet.ts
 
-import { spec } from '@docknetwork/node-types';
+import { spec } from "@docknetwork/node-types";
 
 export default { typesBundle: { spec: spec } };
