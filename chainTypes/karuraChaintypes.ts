23b08bf0a86178bd4c9e6d20897fb0631019c1b8
JQQQ <jiqiang90@hotmail.com>
2024-08-02 11:04:05 +1200

    Update karura chainTypes at 44075

--- chainTypes/karuraChaintypes.ts
+++ chainTypes/karuraChaintypes.ts
@@ -1,5 +1,21 @@
 import { acalaTypesBundle as typesBundle } from "@acala-network/types";
 
+// Need to make a copy in order to override
+const overrideTypesBundle = {
+  ...typesBundle,
+};
+overrideTypesBundle.spec.karura.types.push({
+  minmax: [0, undefined],
+  types: {
+    AuthorityOrigin: "DelayedOrigin",
+    DelayedOrigin: {
+      delay: "BlockNumber",
+      origin: "PalletsOrigin",
+    },
+    ScheduleTaskIndex: "u32",
+  },
+});
+
 export default {
-  typesBundle,
+  typesBundle: overrideTypesBundle,
 };
